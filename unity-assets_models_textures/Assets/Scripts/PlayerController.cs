ba967c4b460330d5972dc55272e8a16a8c786175
gomez7373 <sgc.holberton@gmail.com>
2025-06-01 16:27:21 -0400

    files

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+/// <summary>
+/// Controls player movement and jumping.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Movimiento
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        moveInput = new Vector3(moveX, 0f, moveZ).normalized;
+        moveVelocity = moveInput * moveSpeed;
+
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Movimiento sin impulso físico
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Untagged") || other.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+}
