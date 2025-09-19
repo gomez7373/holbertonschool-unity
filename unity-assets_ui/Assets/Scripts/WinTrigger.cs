fa49347cab1be6ee526c2aefb83c06bbd563c11e
gomez7373 <sgc.holberton@gmail.com>
2025-09-19 16:12:46 -0400

    1 2 3 sceen and more

---
+++ unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Stops timer and updates timer text when Player reaches WinFlag.
+/// </summary>
+public class WinTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("🏁 Player reached WinFlag!");
+
+            Timer timer = other.GetComponent<Timer>();
+            if (timer != null)
+            {
+                timer.enabled = false;
+            }
+
+            // Get the TimerText from the Timer script
+            Text timerText = timer.timerText;
+            if (timerText != null)
+            {
+                timerText.fontSize = 60; // Make it bigger
+                timerText.color = Color.green; // Make it green
+            }
+
+            // Optional: disable movement
+            PlayerController controller = other.GetComponent<PlayerController>();
+            if (controller != null)
+            {
+                controller.enabled = false;
+            }
+        }
+    }
+}
