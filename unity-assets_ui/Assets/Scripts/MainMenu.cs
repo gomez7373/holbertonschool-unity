1882fd99f4a542ff4b6589fd2f470869ef00230f
gomez7373 <sgc.holberton@gmail.com>
2025-09-20 03:49:53 -0400

    OPTIONS & EXIT

--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -6,15 +6,20 @@ public class MainMenu : MonoBehaviour
     // This function loads the selected level based on the input parameter
     public void LevelSelect(int level)
     {
+        Debug.Log("Button pressed: Level " + level); // Verification
+
         switch (level)
         {
             case 1:
+                Debug.Log("Trying to load Level01");
                 SceneManager.LoadScene("Level01");
                 break;
             case 2:
+                Debug.Log("Trying to load Level02");
                 SceneManager.LoadScene("Level02");
                 break;
             case 3:
+                Debug.Log("Trying to load Level03");
                 SceneManager.LoadScene("Level03");
                 break;
             default:
@@ -26,7 +31,9 @@ public class MainMenu : MonoBehaviour
     // This function loads the Options scene
     public void Options()
     {
-         // Save the current scene name as the previous scene
+        Debug.Log("Button pressed: Options"); // Verification
+
+        // Save the current scene name as the previous scene
         PlayerPrefs.SetString("PreviousScene", "MainMenu");
         SceneManager.LoadScene("Options");
     }
@@ -34,7 +41,7 @@ public class MainMenu : MonoBehaviour
     // This function closes the game and prints to console
     public void Exit()
     {
-        Debug.Log("Exited");
+        Debug.Log("Button pressed: Exit"); // Verification
         Application.Quit();
     }
 }
