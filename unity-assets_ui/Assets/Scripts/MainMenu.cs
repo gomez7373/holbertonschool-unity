3d7610511188b05df0050664488fdc0de162f1a8
gomez7373 <sgc.holberton@gmail.com>
2025-09-21 23:59:05 -0400

    fix: exit

--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -3,45 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // This function loads the selected level based on the input parameter
+    // Load level scenes
     public void LevelSelect(int level)
     {
-        Debug.Log("Button pressed: Level " + level); // Verification
-
         switch (level)
         {
             case 1:
-                Debug.Log("Trying to load Level01");
                 SceneManager.LoadScene("Level01");
                 break;
             case 2:
-                Debug.Log("Trying to load Level02");
                 SceneManager.LoadScene("Level02");
                 break;
             case 3:
-                Debug.Log("Trying to load Level03");
                 SceneManager.LoadScene("Level03");
-                break;
-            default:
-                Debug.LogWarning("Invalid level number!");
                 break;
         }
     }
 
-    // This function loads the Options scene
+    // Load Options scene
     public void Options()
     {
-        Debug.Log("Button pressed: Options"); // Verification
-
-        // Save the current scene name as the previous scene
-        PlayerPrefs.SetString("PreviousScene", "MainMenu");
         SceneManager.LoadScene("Options");
     }
 
-    // This function closes the game and prints to console
+    // Exit game
     public void Exit()
     {
-        Debug.Log("Button pressed: Exit"); // Verification
+        Debug.Log("Exited");
         Application.Quit();
     }
 }
